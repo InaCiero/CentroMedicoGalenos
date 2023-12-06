ff613e7838f8e7f8e1c35bd9ca5c69b2814ba305
faac1 <fitofavo.2001@gmail.com>
2023-12-06 00:49:01 -0300

    arreglo
    Este es el que esta bonito

--- CentroMedicoGalinos/settings.py
+++ CentroMedicoGalinos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,9 @@ INSTALLED_APPS = [
     'crispy_forms',
     "crispy_bootstrap5",
 
+    'django_bootstrap5',
     'horas',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,8 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# settings.py
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- horas/urls.py
+++ horas/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from horas import views
 
 urlpatterns = [
-    path('',views.ListadoHoras.as_view(), name='horamedica_list' ),
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('vsecretaria',views.ListadoHoras.as_view(), name='horamedica_list' ),
     path('new/',views.AgregarHoras.as_view(), name='horamedica_new' ),
     path('<int:pk>/edit/',views.EditarHoras.as_view(), name='horamedica_edit' ),
-    path('<int:pk>/delete/',views.BorrarHoras.as_view(), name='horamedica_delete' )
+    path('<int:pk>/delete/',views.BorrarHoras.as_view(), name='horamedica_delete' ),
+
+    path('usuarios/', views.ListarUsuarios.as_view(), name='usuario_list'),
+    path('usuarios/new/', views.AgregarUsuario.as_view(), name='usuario_new'),
+    path('usuarios/<int:pk>/edit/', views.EditarUsuario.as_view(), name='usuario_edit'),
+    path('usuarios/<int:pk>/delete/', views.BorrarUsuario.as_view(), name='usuario_delete'),
 ]
